a5aa4532e19ac6ef5b9e55271e5fdaa99754a1e2
telis <telis@telis-ubuntu>
2021-06-30 01:03:38 +0300

    added data

---
+++ data.sql
@@ -0,0 +1,93 @@
+
+
+-- empty out rows
+-- Uncomment the following command if your your MySql session has the 'safe-updates' option set (prevents update or delete of records without specifying a key)
+-- SET SQL_SAFE_UPDATES = 0;
+
+
+DELETE FROM `appointment`;
+DELETE FROM `doctor`;
+DELETE FROM `hospital`;
+DELETE FROM `admin`;
+DELETE FROM `headadmin`;
+DELETE FROM `patient`;
+DELETE FROM `user`;
+
+
+
+INSERT INTO `user` VALUES ('cgallimore0','2C52C69FBD9B7F298BA0A6B663D3C2F1','Bq_&HR`LRkIHdT4_','Clary','Gallimore','patient');				-- password: nYVXInYN
+INSERT INTO `user` VALUES ('egarrity1','090637A1C35474BEBCA9CAA96D6462FC','&X2bSMo@#PE$DeGj','Emelia','Garrity','patient');				-- password: CZQM4K
+INSERT INTO `user` VALUES ('lallberry2','E06AF463ADF5BB3374A1887D82BB2130','&A&b!1#YP!RGd4_]','Lorena','Allberry','patient');				-- password: XAa884V
+INSERT INTO `user` VALUES ('smcdool3','E87F22FC08979BEED0FDF9F05F56304F','dZg}cWVpKOFQlq*j','Stavros','McDool','patient');					-- password: 1eFdyLqQaN
+INSERT INTO `user` VALUES ('tyarnall4','1CBB15D75C2775DF7EBD73517B75F929','nPvc@r5Y8+G7Cp6l','Troy','Yarnall','patient');					-- password: AcEN32HVc2
+INSERT INTO `user` VALUES ('awraxall5','2A46E8200C01093B75456526D8C15FA2','lhR36NPG]n)@`WeL','Alphonse','Wraxall','patient');				-- password: JvBhAeAe
+INSERT INTO `user` VALUES ('ljobling6','B685C6176EB120BAA0173011DB51A643','`2bQ9YaRk}A3faRi','Lindy','Jobling','patient');					-- password: ZqCcl9FMq
+INSERT INTO `user` VALUES ('aspedding7','87BC0FEC560A7C96052A522AA9B54949','E]j4J_GaB5&QpPU3','Aloin','Spedding','patient');				-- password: 0CoRbATiiKS
+INSERT INTO `user` VALUES ('astolze8','F8F2101C9C979B6942A0F1F5D7BD84E8','3NrTJ%7yLF7XgxV*','Archie','Stolze','patient');					-- password: dQ2fEWiIiF
+INSERT INTO `user` VALUES ('rphilippeaux9','A57F5E3C5D5953993F4DD49C88359A28','HEL7ZEYktVI][}Gs','Rutherford','Philippeaux','patient');			-- password: Qn8vEMFvn
+INSERT INTO `user` VALUES ('rgodinga','5C1012D5F981F382667FB0B1FA46DAE5','StAOzRM^yORvH@4O','Robbert','Goding','patient');					-- password: 3CnGZF6
+INSERT INTO `user` VALUES ('mandrenb','2A42B48F15A4CB8421968543D7D28955','keNz8kl0p%2mB)7r','Mehetabel','Andren','patient');				-- password: 4FZBvpDY
+INSERT INTO `user` VALUES ('tbizleyc','0871254DFAB520DD67B5B20CDC4D9325','+k73#TJ4T3qDe`4)','Tatiana','Bizley','patient');					-- password: YEx1bjg9SNx4
+INSERT INTO `user` VALUES ('mbettingtond','31B6DCE7351A43E1D684AD55E18C1926','xKZurQednLy#LBru','Missy','Bettington','patient');				-- password: UrfWM6t
+INSERT INTO `user` VALUES ('msoppe','B22103EFBD672C87B6FB0AF0FBD00D64','1HfPnb}CZ=Khc&@Y','Michele','Sopp','patient');					-- password: H87AYUZV0z
+INSERT INTO `user` VALUES ('bsimonsenf','91A285052239E08FF44499181A9557B8','2lCbRtZ9NImSQ0*B','Balduin','Simonsen','patient');				-- password: HOHzagCsfC
+INSERT INTO `user` VALUES ('tlabuschg','C991BBB0623E93BB1B01BB192B6C3055','s~*`2D@e`O9(%*oy','Tommy','Labusch','patient');					-- password: FGnPsxz8vtC
+INSERT INTO `user` VALUES ('abirchallh','D411B6B2AA49B3A0D0889CD4FA34D7B8','T}}]0g!gn!vU74zJ','Aloysia','Birchall','patient');				-- password: Nv5zF1uvXLR
+INSERT INTO `user` VALUES ('atindleyi','90C7CDD0CEAA91FC722FE9890608CFF3','QT59;eng*;8(BN{G','Alicea','Tindley','patient');				-- password: wJ9I1HhVsUgX
+INSERT INTO `user` VALUES ('kcravenj','857C006E90C19476AB60F494B75F8AD1','O^CbLy4*COnSC0}#','Kristen','Craven','patient');					-- password: oH7GOl
+INSERT INTO `user` VALUES ('rrubinfajnk','A871FF6D12EC65AF0AE3200CB4615519','`EX%hk3CEVAo9cAU','Roxie','Rubinfajn','patient');				-- password: QxxpVFk4PJ
+INSERT INTO `user` VALUES ('rpallesenl','1F10FD539D08F18FD989047D001EAC7F','*cF2b0;7A33^r!8%','Renate','Pallesen','patient');				-- password: LPPKEG04
+INSERT INTO `user` VALUES ('ablumm','4EB7F9D0B47DD30365F8EC16B428B53F','or=1[~N7a3oAuek@','Afton','Blum','patient');					-- password: ok8H3c
+INSERT INTO `user` VALUES ('amegarryn','ED4088EE7ED867FC6EE9FB13359C6E4F','q+peNrgQMm+$fgIy','Ase','Megarry','patient');					-- password: LJV6FUAndw
+INSERT INTO `user` VALUES ('afergusono','DEFEB8F519041D55EC73707261B1EC32','jjmf``hZ2t8H4d1#','Arch','Ferguson','patient');				-- password: KXxjBPWz
+INSERT INTO `user` VALUES ('jmaphamp','E2FC725AC7D5E28E9BA1A54C41675C59','_6+_xic=7+~XIgz6','Janie','Mapham','patient');					-- password: EMbvPWgjMk4
+INSERT INTO `user` VALUES ('ghardypigginq','F34D68D90184B71031F02509D036B302','5jzoK`bE93*MuI;z','Gay','Hardy-Piggin','patient');				-- password: InTcd99yW9D
+INSERT INTO `user` VALUES ('tminsterr','F5AA7470E3A2A383260FCA0DEEBFDA08','hls4cgX(g~[ORv^]','Tomas','Minster','patient');					-- password: cKFCxx1
+INSERT INTO `user` VALUES ('kchamberlens','83B7B73804157FF36A7FB243B81CFC7E','N`JvM$RM=6rLFe5S','Karim','Chamberlen','patient');				-- password: jWgdBW4Hb
+INSERT INTO `user` VALUES ('kcousanst','02D89F1D7C77601F5EC28981D6D207C5','(^4Z(=g1hajoW1la','Keefer','Cousans','patient');				-- password: k8EN2WGGl
+
+
+
+INSERT INTO `user` VALUES ('sscrivineru','CB7795D6C844D99872175B58E9383133','A3}sxGPyrmj~NN%2','Stefan','Scriviner','doctor');				-- password: nX366I
+INSERT INTO `user` VALUES ('fjerransv','F12FD3760AA5FA742E701F3A74E3CC9C','+}Kh*Z_x+C`Jt_mY','Farlie','Jerrans','doctor');					-- password: mELqdn
+INSERT INTO `user` VALUES ('cmacelhargew','09E9028C22CD4A91CF770F49D45C4943','7L]qv*U3nCn#CWy6','Constancia','MacElharge','doctor');			-- password: sVUC0K
+INSERT INTO `user` VALUES ('mwellandx','C9B614548FB5E6305B94725D94E06135',']b}b`G%9Cnf[d]c4','Myrle','Welland','doctor');					-- password: u5Er5Vx
+INSERT INTO `user` VALUES ('ibarberyy','3074FD86B1AFFF8B1A95FBF859D828C6','qQ]7dP)4c;K5M~X`','Isidora','Barbery','doctor');				-- password: bbBcnk
+INSERT INTO `user` VALUES ('mdescroixz','6FD13B8BF0152D63025C7075039ADD29','yEKz&qH3cQB&Ei^T','Melinda','Descroix','doctor');				-- password: RySxKUsX
+INSERT INTO `user` VALUES ('nmeharry10','082F2C578C9582CE7A729DC8D1CE3C26','j&2oip}+M1x7E2xY','Nessie','Meharry','doctor');				-- password: wGd0r6NY
+INSERT INTO `user` VALUES ('bsinkings11','72974E674051815FEE06DC44F929A442','AnQebM}jGXW_SNa9','Buiron','Sinkings','doctor');				-- password: FWwVkNn6caMP
+INSERT INTO `user` VALUES ('agawthrop12','CD58564F9E8026B0A99D7B16D609A8E4','jJt6S41vFfmd*9]T','Alta','Gawthrop','doctor');				-- password: 7i78DvxK3
+INSERT INTO `user` VALUES ('ksaye13','5ADBB09A734ABF40DC747ABA1F275A4F','tB@3)Q%^64vYIE`B','Kingsly','Saye','doctor');					-- password: oTv5Mp
+INSERT INTO `user` VALUES ('pzannini14','7EB33FB19881B632F62E4A916B8C7FC7','}vmP`f=]yueWpiX*','Pail','Zannini','doctor');					-- password: vlJL0E7q18e
+INSERT INTO `user` VALUES ('vnelson15','0FF69D8AA17B81946A6EA794AD381745','WbR)qSyNM`p;I9uK','Vasilis','Nelson','doctor');					-- password: oD7Upepq
+INSERT INTO `user` VALUES ('ftredwell16','7E811263C68F15F0A20F16B0F92DFE14','&((1EIZ2ECtRtS}#','Fredericka','Tredwell','doctor');				-- password: 2gtMr1nlZ6
+INSERT INTO `user` VALUES ('bblumire17','B95DBA835E4983023A2E605DA17A612D','Rr4n+{E+Yv)_c4lt','Benjamin','Blumire','doctor');				-- password: LmioOBH
+INSERT INTO `user` VALUES ('bdevenny18','7500586CED68D06C60050C4481B13B3C','&&JMGj0i;ajb4iO%','Brien','Devenny','doctor');					-- password: uAQfRhJj
+INSERT INTO `user` VALUES ('cpourvoieur19','3E7E7089D6D0899E2C05A4F130128167','{o5cYg2ALD#V[c1U','Chalmers','Pourvoieur','doctor');			-- password: XBOoqs6wl
+INSERT INTO `user` VALUES ('lmorgans1a','E32DEF2BEA5BB4474AF44711830B9F0B','V@k5mVPHu@H{JT`8','Linell','Morgans','doctor');				-- password: RH6gxxU
+INSERT INTO `user` VALUES ('agaspero1b','CD9F5E275F9B75D67CE720214F10250B','ly*avmeX3O*a^_rV','Ashbey','Gaspero','doctor');				-- password: XORms5IgB2e
+INSERT INTO `user` VALUES ('vbolstridge1c','6D0CE0393F05D6D85F2497D6A462F46B','Ve3jGc4272_Uys1r','Viviyan','Bolstridge','doctor');			-- password: 6n7S5hgQw3
+INSERT INTO `user` VALUES ('fguiton1d','EF6452E3C0B3A340BF9998F28B4A2DFF','Fn]AOp03u{V}k2Yg','Fielding','Guiton','doctor');				-- password: RptgxrvKWu
+
+
+
+INSERT INTO `user` VALUES ('preckus1e','4C08B5E1DB161BE8D1DE00631C19FF95','f3vVNl5V{7vhcqyy','Pattin','Reckus','admin');					-- password: tXGRyKDwJ0
+INSERT INTO `user` VALUES ('ibarsby1f','287A70D56F8DBC759593EEC609D91276','Mut1@Q3U!kIluhT0','Ivonne','Barsby','admin');					-- password: MXykUGFh8
+INSERT INTO `user` VALUES ('frackley1g','5509A5576D6925B4CDBCFF926AC931DF',';lYWZ+]{Rt#+Lq7i','Fabian','Rackley','admin');					-- password: nawreB
+INSERT INTO `user` VALUES ('aquene1h','33E26B7D9A97A117F9E6F977285E4D64','g3{cozlMvmqBpIL;','Artus','Quene','admin');					-- password: ZiiZnYQa
+INSERT INTO `user` VALUES ('malwen1i','BAA62D09F22BD79627B6DDB2FE825EF0','vF=!I*tQBpLy88@N','Mavis','Alwen','admin');					-- password: 8QnuUNmzUC7B
+INSERT INTO `user` VALUES ('egaunt1j','74488C7FD02EB96AB14AB5B135ED0A23','K*b(#cujoP1RU4e)','Ebonee','Gaunt','admin');					-- password: i4KLDwEOc
+INSERT INTO `user` VALUES ('tvarah1k','5090E6324B24B6AB53DE8AB1C36A10A1','b1j~HA(Rq^s`#20N','Tera','Varah','admin');					-- password: KhN7g44IK
+INSERT INTO `user` VALUES ('jchristou1l','766F8B24252E9D4D9A61F33EDA0CA1C3','9+ls`=HQ}ZGCK=Yg','Joane','Christou','admin');				-- password: 19VCXC3TP9
+INSERT INTO `user` VALUES ('mkellock1m','0EA490E3B1A51F585F9740B5C86CC826','o9^!S9m]T*2+YHKg','Malchy','Kellock','admin');					-- password: i24KLato
+INSERT INTO `user` VALUES ('pvongrollmann1n','A003246D0A142CC2889159E6A92B7229','^E^KK`(_)LqP}AaJ','Phylys','von','admin');				-- password: jquatum
+
+
+
+INSERT INTO `user` VALUES ('skiwitz1o','343F06B967969D90EDFD3A4546EC8C97','jekXE`]dhZ`LYe79','Sammy','Kiwitz','headadmin');					-- password: iu5DlpMJ9
+INSERT INTO `user` VALUES ('briby1p','A773456FAC3C02FB718BBF4F99BA2E12','_QKF}MlU1k4sKmMI','Brina','Riby','headadmin');					-- password: WoA4jLd8t
+INSERT INTO `user` VALUES ('bkindall1q','485E5A597AEBB71D296F5D5A1B7E8CC1','YOEfTrpq8*m5FhE8','Bruis','Kindall','headadmin');					-- password: PAdiukA
+
+
+
+
