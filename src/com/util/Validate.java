0a91edc67c600f9f642a02e0b684ffa08d81da8e
Tonious <Tonious@DESKTOP-LVCTNH9>
2021-07-03 09:32:49 +0300

    -- methods implemented

--- src/com/util/Validate.java
+++ src/com/util/Validate.java
@@ -2,19 +2,26 @@ package com.util;
 
 public class Validate {
 	
-	public static boolean validPass() {
+	public static boolean validPass(String pass) {
+		// Requirements: minimum 1 lower-case character, 1 upper-case character, 1 digit and minimum length 8
+		
+		// If length < 8 return false
+		if(pass.length() < 8) return false;
+		// If no lower case return false
+		if(pass.toUpperCase().equals(pass)) return false;
+		// If no upper case return false
+		if(pass.toLowerCase().equals(pass)) return false;
+		// If there is at least one digit return true, otherwise return false
+		for(int i = 0; i < pass.length(); i++) {
+			if(Character.isDigit(pass.charAt(i))) return true;
+		}
 		return false;
-		// 1 lower-case, 1 upper-case, 1 number, minimum-length 8
 	};
 	
-	public static boolean validAMKA() {
-		return false;
-		// minimum-length 11
+	public static boolean validAMKA(String amka) {
+		// Requirements: minimum length 11
+		if(amka.length() < 11) return false;
+		else return true;
 	};
-	
-	
-	
-	
-	
 	
 }
